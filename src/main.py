2c63e9d2f346e51ae11e998527b1285f42666139
Gci04 <gcinzoe04@gmail.com>
2020-01-08 14:34:52 +0300

    Visualisation, appliaces states

--- src/main.py
+++ src/main.py
@@ -1,16 +1,13 @@
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
-from utils import *
-
+from utils import get_data
+from models import *
 
 def main():
     (xtrain, ytrain, xtest, ytest) = get_data()
     model = IntervalModelLightGBM()
     model.fit(xtrain,ytrain)
     pred = model.predict(xtest,ytest)
-    model.calculate_errors()
-    model.plot_predictions()
+    # model.calculate_errors()
+    # model.plot_predictions()
 
 if __name__ == '__main__':
     main()
